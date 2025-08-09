c543f9b467db7d3aea83b8bfced44cd68b57f992
Sanad Ashour <sanadashour2002@yahoo.com>
2025-08-09 18:19:24 +0300

    Add files via upload

---
+++ CLUB MEMBERSHIP/ClubClassLibrary/Models/Membership.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubClassLibrary.Models
+{
+    internal class Membership
+    {
+        public int Id { get; set; }
+        public int MemberId { get; set; }
+        public Member Member { get; set; }
+        public int MembershipTypeId { get; set; } //FK
+        public MembershipType MembershipType { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ CLUB MEMBERSHIP/ClubClassLibrary/Models/Payment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubClassLibrary.Models
+{
+    internal class Payment
+    {
+        public int Id { get; set; }
+        public int MemberId { get; set; }
+        public Member Member { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; } = DateTime.Now;
+        public string PaymentMethod { get; set; }
+        public int? MemberShipId { get; set; }
+        public virtual Membership Membership { get; set; }
+    }
+}
