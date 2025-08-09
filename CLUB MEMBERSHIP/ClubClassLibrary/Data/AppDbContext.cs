1e279df4e77a90643352e091188c5364098c1bc0
Sanad Ashour <sanadashour2002@yahoo.com>
2025-08-10 00:26:34 +0300

    Add files via upload

---
+++ CLUB MEMBERSHIP/ClubClassLibrary/Data/AppDbContext.cs
@@ -0,0 +1,70 @@
+﻿using ClubClassLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubClassLibrary.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Membership> Memberships { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=.\SQLEXPRESS;database=CLUB;integrated security=true;TrustServerCertificate=true");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Member - Membership (one to many)
+            modelBuilder.Entity<Membership>()
+                .HasOne(m => m.Member)
+                .WithMany(mb => mb.Memberships)
+                .HasForeignKey(m => m.MemberId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            //MembershipType - Membership (one to many)
+            modelBuilder.Entity<Membership>()
+                .HasOne(m => m.MembershipType)
+                .WithMany(mt => mt.Memberships)
+                .HasForeignKey(m => m.MembershipTypeId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            //Member - Payment (one to many)
+            modelBuilder.Entity<Payment>()
+                .HasOne(p => p.Member)
+                .WithMany(m => m.Payments)
+                .HasForeignKey(p => p.MemberId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            //Membership - Payment (one to many)
+            modelBuilder.Entity<Payment>()
+                .HasOne(p => p.Membership)
+                .WithMany(m => m.Payments)
+                .HasForeignKey(p => p.MemberShipId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+
+            //SEEDING MEMBERSHIPS
+            modelBuilder.Entity<MembershipType>().HasData(
+                new MembershipType { Id = 1, Name = "Monthly", Description="A MONTHLY DESCRIPTION", BasePrice=50 },
+                new MembershipType { Id = 2, Name = "Annual", Description = "AN ANNUAL DESCRIPTION", BasePrice = 200 },
+                new MembershipType { Id = 3, Name = "Premium", Description = "A PREMIUM DESCRIPTION", BasePrice = 500 }
+                );
+
+            //SEEDING MEMBERS
+            modelBuilder.Entity<Member>().HasData(
+                new Member {Id=1, PhoneNumber = "1234", Name="Sanad", Address="SARAJ", BirthDate= new DateTime(2002,11,10), JoinDate= new DateTime(2024, 01, 01) },
+                new Member { Id = 2, PhoneNumber="1234", Name = "Seraj", Address = "SARAJ", BirthDate = new DateTime(2004, 10, 24), JoinDate = new DateTime(2024, 01, 01) }
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ CLUB MEMBERSHIP/ClubClassLibrary/Migrations/20250809211205_init.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ClubClassLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JoinDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MembershipTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BasePrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MembershipTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Memberships",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MemberId = table.Column<int>(type: "int", nullable: false),
+                    MembershipTypeId = table.Column<int>(type: "int", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Memberships", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Memberships_Members_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Members",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Memberships_MembershipTypes_MembershipTypeId",
+                        column: x => x.MembershipTypeId,
+                        principalTable: "MembershipTypes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MemberId = table.Column<int>(type: "int", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PaymentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PaymentMethod = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MemberShipId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payments_Members_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Members",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Payments_Memberships_MemberShipId",
+                        column: x => x.MemberShipId,
+                        principalTable: "Memberships",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Members",
+                columns: new[] { "Id", "Address", "BirthDate", "JoinDate", "Name", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { 1, "SARAJ", new DateTime(2002, 11, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Sanad", "1234" },
+                    { 2, "SARAJ", new DateTime(2004, 10, 24, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Seraj", "1234" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "MembershipTypes",
+                columns: new[] { "Id", "BasePrice", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, 50m, "A MONTHLY DESCRIPTION", "Monthly" },
+                    { 2, 200m, "AN ANNUAL DESCRIPTION", "Annual" },
+                    { 3, 500m, "A PREMIUM DESCRIPTION", "Premium" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Memberships_MemberId",
+                table: "Memberships",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Memberships_MembershipTypeId",
+                table: "Memberships",
+                column: "MembershipTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_MemberId",
+                table: "Payments",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_MemberShipId",
+                table: "Payments",
+                column: "MemberShipId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.DropTable(
+                name: "Memberships");
+
+            migrationBuilder.DropTable(
+                name: "Members");
+
+            migrationBuilder.DropTable(
+                name: "MembershipTypes");
+        }
+    }
+}
