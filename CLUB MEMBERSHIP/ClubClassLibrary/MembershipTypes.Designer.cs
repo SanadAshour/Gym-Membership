d30341e3b11d090308205666a8a455ba94f00be6
Sanad Ashour <sanadashour2002@yahoo.com>
2025-08-11 15:49:38 +0300

    MembershipTypes.Designer.cs

--- CLUB MEMBERSHIP/ClubClassLibrary/MembershipTypes.Designer.cs
+++ CLUB MEMBERSHIP/ClubClassLibrary/MembershipTypes.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ClubUI
+namespace ClubUI
 {
     partial class frmMembershipTypes
     {
@@ -29,23 +29,107 @@
         private void InitializeComponent()
         {
             DGV = new DataGridView();
+            MstNameTB = new TextBox();
+            MstDescriptionTB = new TextBox();
+            MstBasePriceTB = new TextBox();
+            addBtn = new Button();
+            updateBtn = new Button();
+            deleteBtn = new Button();
+            clearBtn = new Button();
             ((System.ComponentModel.ISupportInitialize)DGV).BeginInit();
             SuspendLayout();
             // 
             // DGV
             // 
+            DGV.AllowUserToAddRows = false;
+            DGV.AllowUserToDeleteRows = false;
+            DGV.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            DGV.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            DGV.BackgroundColor = SystemColors.ControlLight;
             DGV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            DGV.Location = new Point(30, 12);
+            DGV.Location = new Point(30, 226);
+            DGV.MultiSelect = false;
             DGV.Name = "DGV";
+            DGV.ReadOnly = true;
             DGV.RowHeadersWidth = 51;
-            DGV.Size = new Size(738, 414);
+            DGV.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            DGV.Size = new Size(738, 200);
             DGV.TabIndex = 0;
+            // 
+            // MstNameTB
+            // 
+            MstNameTB.Location = new Point(30, 27);
+            MstNameTB.Name = "MstNameTB";
+            MstNameTB.PlaceholderText = "Name";
+            MstNameTB.Size = new Size(173, 27);
+            MstNameTB.TabIndex = 1;
+            // 
+            // MstDescriptionTB
+            // 
+            MstDescriptionTB.Location = new Point(30, 75);
+            MstDescriptionTB.Name = "MstDescriptionTB";
+            MstDescriptionTB.PlaceholderText = "Description";
+            MstDescriptionTB.Size = new Size(173, 27);
+            MstDescriptionTB.TabIndex = 2;
+            // 
+            // MstBasePriceTB
+            // 
+            MstBasePriceTB.Location = new Point(30, 121);
+            MstBasePriceTB.Name = "MstBasePriceTB";
+            MstBasePriceTB.PlaceholderText = "Base Price";
+            MstBasePriceTB.Size = new Size(173, 27);
+            MstBasePriceTB.TabIndex = 3;
+            // 
+            // addBtn
+            // 
+            addBtn.Location = new Point(122, 170);
+            addBtn.Name = "addBtn";
+            addBtn.Size = new Size(94, 43);
+            addBtn.TabIndex = 4;
+            addBtn.Text = "ADD";
+            addBtn.UseVisualStyleBackColor = true;
+            addBtn.Click += addBtn_Click;
+            // 
+            // updateBtn
+            // 
+            updateBtn.Location = new Point(252, 170);
+            updateBtn.Name = "updateBtn";
+            updateBtn.Size = new Size(94, 43);
+            updateBtn.TabIndex = 5;
+            updateBtn.Text = "UPDATE";
+            updateBtn.UseVisualStyleBackColor = true;
+            // 
+            // deleteBtn
+            // 
+            deleteBtn.Location = new Point(393, 170);
+            deleteBtn.Name = "deleteBtn";
+            deleteBtn.Size = new Size(94, 43);
+            deleteBtn.TabIndex = 6;
+            deleteBtn.Text = "DELETE";
+            deleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // clearBtn
+            // 
+            clearBtn.Location = new Point(533, 170);
+            clearBtn.Name = "clearBtn";
+            clearBtn.Size = new Size(94, 43);
+            clearBtn.TabIndex = 7;
+            clearBtn.Text = "CLEAR";
+            clearBtn.UseVisualStyleBackColor = true;
+            clearBtn.Click += clearBtn_Click;
             // 
             // frmMembershipTypes
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(clearBtn);
+            Controls.Add(deleteBtn);
+            Controls.Add(updateBtn);
+            Controls.Add(addBtn);
+            Controls.Add(MstBasePriceTB);
+            Controls.Add(MstDescriptionTB);
+            Controls.Add(MstNameTB);
             Controls.Add(DGV);
             Name = "frmMembershipTypes";
             StartPosition = FormStartPosition.CenterScreen;
@@ -53,10 +137,18 @@
             Load += MembershipTypes_Load;
             ((System.ComponentModel.ISupportInitialize)DGV).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView DGV;
+        private TextBox MstNameTB;
+        private TextBox MstDescriptionTB;
+        private TextBox MstBasePriceTB;
+        private Button addBtn;
+        private Button updateBtn;
+        private Button deleteBtn;
+        private Button clearBtn;
     }
 }
