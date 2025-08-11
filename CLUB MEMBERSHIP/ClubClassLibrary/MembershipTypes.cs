cc9089ec6ad85caddec0ebd092c189d28b21e27b
Sanad Ashour <sanadashour2002@yahoo.com>
2025-08-11 15:48:22 +0300

    MembershipTypes.cs

--- CLUB MEMBERSHIP/ClubClassLibrary/MembershipTypes.cs
+++ CLUB MEMBERSHIP/ClubClassLibrary/MembershipTypes.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,21 +8,88 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ClubClassLibrary.Data;
+using ClubClassLibrary.Repositories;
+using ClubClassLibrary.Models;
+
 
 namespace ClubUI
 {
     public partial class frmMembershipTypes : Form
     {
+        MembershipTypeRepository repoMembershipType = new MembershipTypeRepository();
         public frmMembershipTypes()
         {
             InitializeComponent();
         }
 
+        private async void ViewMembershipTypes()
+        {
+            var Data = repoMembershipType.GetAllMembershipTypes();
+            DGV.DataSource = Data.Select(mt => new
+            {
+                mt.Name,
+                mt.Description,
+                mt.BasePrice
+            }).ToList();
+
+            DGV.Columns[0].HeaderText = "Membership Name";
+            DGV.Columns[1].HeaderText = "Description";
+            DGV.Columns[2].HeaderText = "Membership Price";
+        }
+
         private void MembershipTypes_Load(object sender, EventArgs e)
         {
-            var repo = new MembershipTypeRepository();
-            DGV.DataSource = repo.GetAllMembershipTypes();
+            ViewMembershipTypes();
+        }
+
+        private bool ValidateData()
+        {
+            bool valid = false;
+            if (MstNameTB.Text == "")
+            {
+                valid = true;
+            }
+            if (MstDescriptionTB.Text == "")
+            {
+                valid = true;
+            }
+            if (MstBasePriceTB.Text == "")
+            {
+                valid = true;
+            }
+
+            return valid;
+        }
+
+        private void ClearData()
+        {
+            MstNameTB.Text = "";
+            MstDescriptionTB.Text = "";
+            MstBasePriceTB.Text = "";
+        }
+
+        private async void addBtn_Click(object sender, EventArgs e)
+        {
+            if (ValidateData())
+            {
+                return;
+            }
+
+            await repoMembershipType.AddMembershipTypeAsync(new MembershipType
+            {
+                Name = MstNameTB.Text,
+                Description = MstDescriptionTB.Text,
+                BasePrice = decimal.Parse(MstBasePriceTB.Text)
+            });
+
+            ClearData();
+            MessageBox.Show("ADDED SUCCESSFULLY!", "CONFIRMATION", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ViewMembershipTypes();
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            ClearData();
         }
     }
 }
-
