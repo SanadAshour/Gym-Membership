7d48a7d699759fd663c78e8a069457e0ff2a0944
Sanad Ashour <sanadashour2002@yahoo.com>
2025-08-16 22:49:38 +0300

    frmMemberships.cs

--- CLUB MEMBERSHIP/ClubClassLibrary/frmMemberships.cs
+++ CLUB MEMBERSHIP/ClubClassLibrary/frmMemberships.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClubClassLibrary.Repositories;
+using ClubUI.ViewModels;
 
 namespace ClubUI
 {
     public partial class frmMemberships : Form
     {
+        MemberRepository repoMember = new MemberRepository();
+        MembershipTypeRepository repoMembershipType = new MembershipTypeRepository();
+        MembershipRepository repoMembership = new MembershipRepository();
+        private int SelectedId;
         public frmMemberships()
         {
             InitializeComponent();
+            FillMemberCB();
+            FillMembershipTypeCB();
+            LoadMembershipsDGV();
+        }
+
+        private void FillMemberCB()
+        {
+            MemberCB.DataSource = repoMember.GetAllMembers();
+            MemberCB.ValueMember = "Id";
+            MemberCB.DisplayMember = "Name";
+        }
+
+        private void FillMembershipTypeCB()
+        {
+            MembershipTypeCB.DataSource = repoMembershipType.GetAllMembershipTypes();
+            MembershipTypeCB.ValueMember = "Id";
+            MembershipTypeCB.DisplayMember = "Name";
+        }
+        
+        private void LoadMembershipsDGV()
+        {   
+            var data = repoMembership.GetAll();
+            DGV.DataSource = data.Select(x => new MembershipViewModel{
+                Id = x.Id,
+                MemberName = x.Member.Name,
+                MembershipTypeName = x.MembershipType.Name,
+                StartDate = x.StartDate,
+                EndDate = x.EndDate,
+                Price = x.Price,
+            }).ToList();
+            DGV.Columns[0].Visible = false;
+            DGV.Columns[1].HeaderText = "Name";
+            DGV.Columns[2].HeaderText = "Membership";
+            DGV.Columns[3].HeaderText = "Start Date";
+            DGV.Columns[4].HeaderText = "End Date";
+            DGV.Columns[5].HeaderText = "Price";
         }
     }
 }
