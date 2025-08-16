faa901cb52ee2ee930099d77f1eb100b19d96448
Sanad Ashour <sanadashour2002@yahoo.com>
2025-08-16 22:52:05 +0300

    Add files via upload

---
+++ CLUB MEMBERSHIP/ClubClassLibrary/ViewModels/MembershipViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ClubClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubUI.ViewModels
+{
+    internal class MembershipViewModel
+    {
+        public int Id { get; set; }
+        public string MemberName { get; set; }
+        public string MembershipTypeName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
